0cea2c59cf054337a374f0ebcaaa6c9c1c352536
Joel Fernandes <joelaf@google.com>
2017-09-11 17:05:49 -0700

    ANDROID: sched/rt: add schedtune accounting
    This patch adds schedtune enqueue/dequeue to RT scheduling class.
    
    Change-Id: If416e64319d62191f3aedd675d3e9a21fe2102fb
    Signed-off-by: Joel Fernandes <joelaf@google.com>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1505,6 +1505,7 @@ enqueue_task_rt(struct rq *rq, struct task_struct *p, int flags)
 	schedtune_enqueue_task(p, cpu_of(rq));
 	sched_rt_update_capacity_req(rq);
 	cpufreq_update_this_cpu(rq, SCHED_CPUFREQ_RT);
+	schedtune_enqueue_task(p, cpu_of(rq));
 }
 
 static void dequeue_task_rt(struct rq *rq, struct task_struct *p, int flags)
@@ -1531,6 +1532,7 @@ static void dequeue_task_rt(struct rq *rq, struct task_struct *p, int flags)
 	schedtune_dequeue_task(p, cpu_of(rq));
 	sched_rt_update_capacity_req(rq);
 	cpufreq_update_this_cpu(rq, SCHED_CPUFREQ_RT);
+	schedtune_dequeue_task(p, cpu_of(rq));
 }
 
 /*
